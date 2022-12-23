449b95ff819f363d39d467333a31a950ad39557e
Li <peiyan.li@powertechlabs.com>
2022-12-22 21:00:02 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+This is a python script allowing to set any window stick to the top (front) in the desktop of Windows OS. The `pystray` module is required.
+
+# Usage
+Running the script will create a tray icon.  Select a window; Right-click the icon to check, set, and unset the "always-on-top" status.
---
+++ SetWndAtTop.py
@@ -0,0 +1,62 @@
+import pystray
+import win32gui, win32con
+import time
+import sys
+import os
+from threading import Thread
+from PIL import Image
+
+status = ["Windows status ", ""]
+flags = [True]
+hdl = [0]
+
+
+image = Image.open(os.path.join(os.path.dirname(__file__), 'setWndAtTop.png'))
+icon = pystray.Icon("Always At Top")
+icon.icon = image
+
+def update_status():
+    i = win32gui.GetForegroundWindow()
+    s = win32gui.GetWindowText(i)
+    if (s.strip()):
+        style = win32gui.GetWindowLong(hdl[0], win32con.GWL_EXSTYLE)
+        if (style & win32con.WS_EX_TOPMOST):
+            status[0] = "At Top"
+            status[1] = s
+            hdl[0] = i
+        else:
+            status[0] = "Not At Top"
+            status[1] = s
+            hdl[0] = i
+    icon.update_menu()
+
+def set_active_window_alwaytop():
+    [x, y, x1, y1] = win32gui.GetWindowRect(hdl[0])
+    win32gui.SetWindowPos(hdl[0], win32con.HWND_TOPMOST, x, y, x1-x, y1-y, 0)
+
+def set_active_window_not_alwaytop():
+    [x, y, x1, y1] = win32gui.GetWindowRect(hdl[0])
+    win32gui.SetWindowPos(hdl[0], win32con.HWND_NOTOPMOST, x, y, x1-x, y1-y, 0)
+
+def stop():
+    flags[0] = False
+    icon.stop()
+
+item_item1 = pystray.MenuItem(lambda text: status[0] + " - " + status[1], lambda : None)
+item_item2 = pystray.MenuItem("Set Always On Top", set_active_window_alwaytop)
+item_item3 = pystray.MenuItem("Set Not Always On Top", set_active_window_not_alwaytop)
+item_item4 = pystray.MenuItem("Exit", stop)
+menu = pystray.Menu(item_item1, item_item2, item_item3, item_item4)
+
+icon.menu = menu
+
+def get_active_window_status():
+    while flags[0]:
+        update_status()
+        time.sleep(1)
+
+thread1 = Thread(target = get_active_window_status)
+thread1.start()
+icon.run()
+
+
